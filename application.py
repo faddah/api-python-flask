466de37cb307783280f40fd349d2987400ac013b
Faddah Wolf <my_biz@me.com>
2025-01-30 18:39:36 -0800

    Created Python Flask server for APIs in application.py.

--- application.py
+++ application.py
@@ -5,4 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '<div style="text-align: center; margin: auto auto;"><h1>Hello, Ya Crazy Muthafuggahz‼️</h1></div>'
+    return '<div style="text-align: center; margin: 7.5%;"><h1>Hello, Ya Crazy Muthafuggahz‼️</h1></div>'
+
+
+@app.route("/drinks")
+def get_index():
+
+    return {"drinks": ["water", "soda", "juice"]}
