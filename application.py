2d40d3ffc7e757949b900d8dc81c99d69f204725
Faddah Wolf <my_biz@me.com>
2025-01-30 20:05:59 -0800

    Added import of SQLAlchemy at top; added code to configure & instantiate sqlite db; and, added the db.Model for the Drink table in db.

--- application.py
+++ application.py
@@ -1,13 +1,21 @@
-import sqlite3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 # import the necessary Python module for db
 
 app = Flask(__name__)
 
-class Drink:
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
 
 
 @app.route("/")
